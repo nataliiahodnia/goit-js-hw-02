9bf1cbf4b85a5e7a0df13934825eaaf723e15977
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-03-06 22:25:32 +0100

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+// Оголошення параметрів quantity, pricePerDroid
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You bought ${quantity} droids, total price is ${totalPrice} credits.`;
+}
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
